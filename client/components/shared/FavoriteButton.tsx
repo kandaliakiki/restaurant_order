3deee18344cb795e3f0cf3bbf74e27c3903147c8
kandaliakiki <kandaliak1k1@gmail.com>
2024-08-13 16:35:21 +0700

    more animation favorite button

--- client/components/shared/FavoriteButton.tsx
+++ client/components/shared/FavoriteButton.tsx
@@ -8,27 +8,32 @@ import animationData from "../../public/animations/heart-animation.json";
 import HeartAnimation from "./HeartAnimation";
 
 const FavoriteButton = () => {
-  const [isFavorite, setIsFavorite] = useState(false);
-  const animationData = isFavorite ? "/animations/heart-animation.json" : null;
+  const [isFavorite, setIsFavorite] = useState(true);
+  const [animate, setAnimate] = useState(false); // New state for animation control
 
-  const options = {
-    animationData: animationData,
-    loop: true,
+  const handleClick = () => {
+    setIsFavorite(!isFavorite);
+    setAnimate(true); // Trigger animation on click
   };
-
-  const { View } = useLottie(options);
 
   return (
     <Button
       variant="default"
       className="text-[0.8rem] h-[1.5rem] w-[2rem]  px-[0.3rem] bg-white-background flex items-center"
-      onClick={() => setIsFavorite(!isFavorite)}
+      onClick={handleClick}
     >
-      {isFavorite ? (
+      {isFavorite && animate ? ( // Check for animation state
         <HeartAnimation />
+      ) : isFavorite ? ( // Check if isFavorite is true
+        <Image
+          alt="heart-icon-filled"
+          src="/assets/heart-active-2.svg" // New source for filled heart
+          width={18}
+          height={18}
+        />
       ) : (
         <Image
-          alt="heart-icon"
+          alt="heart-icon-outline"
           src="/assets/heart-pink-outline.svg"
           width={18}
           height={18}
