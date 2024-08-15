273cd7966eec99a74b5c25502fdc15e99f43c125
kandaliakiki <kandaliak1k1@gmail.com>
2024-08-15 22:07:07 +0700

    add notification page

---
+++ client/app/(root)/notification/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <section className="flex flex-col gap-2 mt-2">
+      <p className="text-2xl font-medium">Notifications</p>
+      <p className="text-center text-xl">Coming Soon...</p>
+    </section>
+  );
+};
+
+export default page;
