114c37c8308f1cd3b7aef1a43659b344d00bc07c
kandaliakiki <kandaliak1k1@gmail.com>
2024-08-17 12:33:00 +0700

    done food category responsive

--- client/app/(root)/categories/[category]/page.tsx
+++ client/app/(root)/categories/[category]/page.tsx
@@ -7,8 +7,10 @@ import React from "react";
 const page = ({ params }: { params: { category: string } }) => {
   return (
     <section className="flex flex-col gap-2 mt-2">
-      <p className="text-2xl font-medium">Categories</p>
-      <div className="flex gap-2 overflow-x-auto hide-scrollbar">
+      <p className="text-2xl font-medium md:text-3xl xl:text-4xl md:mb-3 ">
+        Categories
+      </p>
+      <div className="flex gap-2 overflow-x-auto hide-scrollbar min-[400px]:justify-center min-[400px]:gap-3 min-[400px]:mb-3">
         {foodCategories.map((category) => (
           <Link key={category.label} href={`/categories/${category.route}`}>
             <Button
@@ -16,7 +18,7 @@ const page = ({ params }: { params: { category: string } }) => {
                 category.route === params.category
                   ? "bg-vibrant-pink text-white"
                   : "bg-white text-gray-disabled"
-              } font-normal flex items-center rounded-md p-1 text-[0.8rem] px-6 h-[1.7rem]`}
+              } font-normal flex items-center rounded-md p-1 text-[0.8rem] px-6 h-[1.7rem] md:text-base md:px-8 md:h-[2rem] lg:text-lg lg:px-10 lg:h-[2.2rem] xl:text-2xl xl:px-12 xl:h-[2.5rem]`}
             >
               {category.label}
             </Button>
